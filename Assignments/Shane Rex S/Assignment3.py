7976978f70c31d925d1a0ac7fea9cee60fe20d40
ShaneRex <shanerex2001@gmail.com>
2022-10-16 00:21:16 +0530

    Add Shane Rex's Assignment 3

---
+++ Assignments/Shane Rex S/Assignment3.py
@@ -0,0 +1,32 @@
+import time
+import RPi.GPIO as GPIO
+from gpiozero import Button, TrafficLights, Buzzer
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+
+while True:
+    GPIO.output(13,True)
+    time.sleep(3)
+    GPIO.output(13,False)
+    time.sleep(3)
+
+buzzer = Buzzer(15)
+button = Button(21)
+lights = TrafficLights(25, 8, 7) #Red Orange Green LED connected to GPIO board
+
+while True:
+    #Traffic Signal Sequence
+    button.wait_for_press()
+    light.green.on()
+    sleep(3)
+    lights.amber.on()
+    sleep(3)
+    lights.red.on()
+    sleep(3)
+    lights.off()
+
+    #Pedestrian Crossing Buzzer
+    buzzer.on()
+    sleep(3)
+    buzzer.off()
